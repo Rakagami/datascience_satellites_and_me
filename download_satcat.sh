7fed6f27e16f09182f2698bef3489b3cac4f003b
Prakita Renatin <ge25zow@mytum.de>
2022-11-29 11:57:20 +0100

    Big Update
    - added Celestrack supplemental data to download script
    - added orbit_display notebook to generate constellation graphs
    - added Notebook to create histograms about constellations

--- download_satcat.sh
+++ download_satcat.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 wget https://celestrak.org/pub/satcat.csv
+
+CONSTELLATIONS=( starlink oneweb planet iridium gps glonass meteosat intelsat ses telesat orbcomm )
+
+mkdir -p constellations
+
+for i in "${CONSTELLATIONS[@]}"
+do
+    curl --output constellations/$i.csv "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=$i&FORMAT=csv"
+    curl --output constellations/$i.xml "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=$i&FORMAT=xml"
+done
